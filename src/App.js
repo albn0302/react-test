6e4b289f055456eda2394b1bea81310c1b1d2722
albn0302 <albin.johansson0302@gmail.com>
2023-02-01 10:24:47 +0200

    Added variables and a console.log to log output.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [done, setDone] = useState([[]]);
+
+  function handleSubmit(event) {
+    // Prevent the browser from reloading the page
+    event.preventDefault();
+
+    // Read the form data
+    const newTodo = event.target.newtodo.value;
+
+    console.log(newTodo);
+  }
+
   return (
     <main>
       <h1>ToDo</h1>
-      <input type="text" placeholder="New todo" v-model="newTodo"></input>
-
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="newtodo" placeholder="New todo" v-model="newTodo"></input>
+      </form>
       <ul id="todo">
         <li>
           <p>Hello!</p>
